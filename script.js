c7124b56721388b6ff4a2c924666297b9705ea77
ugurakcora <uakcora97@gmail.com>
2023-07-07 12:56:00 +0300

    refactor js

--- script.js
+++ script.js
@@ -1,90 +1,101 @@
-// Api
-const countryApi = async (event) => {
-  const api = `https://restcountries.com/v3.1/all`;
-  const countries = document.querySelector(".countries");
-  const regionSelect = document.getElementById("region-select");
-  const searchInput = document.getElementById("search-input");
-  const backButton = document.getElementById("backButton");
-  const modal = document.getElementById("modal");
+const apiUrl = "https://restcountries.com/v3.1/all";
+const countriesContainer = document.querySelector(".countries");
+const regionSelect = document.getElementById("region-select");
+const searchInput = document.getElementById("search-input");
+const backButton = document.getElementById("backButton");
+const modal = document.getElementById("modal");
 
+const fetchData = async () => {
   try {
-    const response = await fetch(api);
+    const response = await fetch(apiUrl);
     if (!response.ok) {
       throw new Error("Hata: Veri alınamadı");
     }
     const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Hata:", error);
+    throw error;
+  }
+};
 
-    console.log(data);
+const filterCountries = (countries, selectedRegion, searchValue) => {
+  return countries.filter((country) => {
+    const countryRegion = country.region.toLowerCase();
+    const countryName = country.name.common.toLowerCase();
+
+    return (
+      (selectedRegion === "" || countryRegion === selectedRegion) &&
+      (searchValue === "" || countryName.includes(searchValue))
+    );
+  });
+};
+
+const renderCountries = (filteredData) => {
+  countriesContainer.innerHTML = "";
+
+  if (filteredData.length === 0) {
+    const noResultsMessage = document.createElement("p");
+    noResultsMessage.classList.add("no-results-message");
+    noResultsMessage.textContent = "Sonuç Bulunamadı";
+    countriesContainer.appendChild(noResultsMessage);
+    return;
+  }
+
+  filteredData.forEach((element) => {
+    const { name, population, region, capital, flags } = element;
+
+    const country = document.createElement("article");
+    country.classList.add("country");
+
+    const imageBtn = document.createElement("button");
+    const countryDetails = document.createElement("div");
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+
+    img.src = flags.svg;
+    img.alt = `${name.common}'s flag`;
+    imageBtn.appendChild(figure);
+    figure.appendChild(img);
+
+    countryDetails.innerHTML = `
+      <h2>${name.common}</h2>
+      <p>Population: ${population.toLocaleString()}</p>
+      <p>Region: ${region}</p>
+      ${capital ? `<p>Capital: ${capital}</p>` : ""}
+    `;
+
+    country.appendChild(imageBtn);
+    country.appendChild(countryDetails);
+    countriesContainer.appendChild(country);
+
+    imageBtn.addEventListener("click", function () {
+      showModal(element, filteredData);
+    });
+  });
+};
+
+const countryApi = async () => {
+  try {
+    const data = await fetchData();
 
     modal.style.display = "none";
 
     let filteredData = data;
 
-    const filterCountries = () => {
+    const filterCountriesHandler = () => {
       const selectedRegion = regionSelect.value.toLowerCase();
       const searchValue = searchInput.value.toLowerCase();
 
-      filteredData = data.filter((country) => {
-        const countryRegion = country.region.toLowerCase();
-        const countryName = country.name.common.toLowerCase();
+      filteredData = filterCountries(data, selectedRegion, searchValue);
 
-        return (
-          (selectedRegion === "" || countryRegion === selectedRegion) &&
-          (searchValue === "" || countryName.includes(searchValue))
-        );
-      });
-
-      renderCountries();
+      renderCountries(filteredData);
     };
 
-    regionSelect.addEventListener("change", filterCountries);
-    searchInput.addEventListener("input", filterCountries);
+    regionSelect.addEventListener("change", filterCountriesHandler);
+    searchInput.addEventListener("input", filterCountriesHandler);
 
-    const renderCountries = () => {
-      countries.innerHTML = "";
-
-      if (filteredData.length === 0) {
-        const noResultsMessage = document.createElement("p");
-        noResultsMessage.classList.add("no-results-message");
-        noResultsMessage.textContent = "Sonuç Bulunamadı";
-        countries.appendChild(noResultsMessage);
-        return; // Sonuç bulunamadığında diğer işlemlere devam etmeyin
-      }
-
-      filteredData.forEach((element) => {
-        const { name, population, region, capital, flags } = element;
-
-        let country = document.createElement("article");
-        country.classList.add("country");
-
-        let imageBtn = document.createElement("button");
-        let countryDetails = document.createElement("div");
-        let figure = document.createElement("figure");
-        let img = document.createElement("img");
-
-        img.src = flags.svg;
-        img.alt = `${name.common}'s flag`;
-        imageBtn.appendChild(figure);
-        figure.appendChild(img);
-
-        countryDetails.innerHTML = `
-          <h2>${name.common}</h2>
-          <p>Population: ${population.toLocaleString()}</p>
-          <p>Region: ${region}</p>
-          ${capital ? `<p>Capital: ${capital}</p>` : ""}
-        `;
-
-        country.appendChild(imageBtn);
-        country.appendChild(countryDetails);
-        countries.appendChild(country);
-
-        imageBtn.addEventListener("click", function () {
-          showModal(element, filteredData); // filteredData parametresi eklendi
-        });
-      });
-    };
-
-    renderCountries();
+    renderCountries(filteredData);
   } catch (error) {
     console.error("Hata:", error);
     const filter = document.querySelector(".filters");
@@ -95,17 +106,12 @@ const countryApi = async (event) => {
     errorMessage.classList.add("no-results-message");
     errorMessage.textContent =
       "Servis hatası. Lütfen daha sonra tekrar deneyin.";
-    countries.appendChild(errorMessage);
+    countriesContainer.appendChild(errorMessage);
   }
 };
 
-// Modal Açmak için
 const showModal = (element, filteredData) => {
-  const modal = document.getElementById("modal");
   const modalContent = document.getElementById("modalContent");
-  const countries = document.querySelector(".countries");
-  const filter = document.querySelector(".filters");
-  modalContent.innerHTML = "";
 
   const {
     name,
@@ -119,6 +125,15 @@ const showModal = (element, filteredData) => {
     borders,
   } = element;
 
+  modalContent.innerHTML = "";
+
+  const createParagraph = (className, textContent) => {
+    const paragraph = document.createElement("p");
+    paragraph.classList.add(className);
+    paragraph.textContent = textContent;
+    return paragraph;
+  };
+
   const countryName = document.createElement("h2");
   countryName.textContent = name.common;
 
@@ -131,45 +146,55 @@ const showModal = (element, filteredData) => {
 
   const infoDiv = document.createElement("div");
   infoDiv.classList.add("info");
-  const nativeName = document.createElement("p");
-  nativeName.textContent = `Native Name: ${name.official}`;
 
-  const populationPara = document.createElement("p");
-  populationPara.textContent = `Population: ${population.toLocaleString()}`;
+  infoDiv.appendChild(countryName);
+  infoDiv.appendChild(
+    createParagraph("native-name", `Native Name: ${name.official}`)
+  );
+  infoDiv.appendChild(
+    createParagraph("population", `Population: ${population.toLocaleString()}`)
+  );
+  infoDiv.appendChild(createParagraph("region", `Region: ${region}`));
+  infoDiv.appendChild(createParagraph("subregion", `Sub Region: ${subregion}`));
 
-  const regionPara = document.createElement("p");
-  regionPara.textContent = `Region: ${region}`;
-
-  const subRegionPara = document.createElement("p");
-  subRegionPara.textContent = `Sub Region: ${subregion}`;
-
-  const capitalPara = document.createElement("p");
-  if (typeof capital !== "undefined" && capital !== null) {
-    capitalPara.textContent = `Capital: ${capital}`;
+  if (capital) {
+    infoDiv.appendChild(createParagraph("capital", `Capital: ${capital}`));
   }
 
-  const domainPara = document.createElement("p");
-  domainPara.textContent = `Top Level Domain: ${element.tld}`;
-
-  const currencyNames = Object.values(currencies).map(
-    (currency) => currency.name
-  );
-  const currencySymbols = Object.values(currencies).map(
-    (currency) => currency.symbol
+  infoDiv.appendChild(
+    createParagraph("tld", `Top Level Domain: ${element.tld}`)
   );
 
-  const currenciesPara = document.createElement("p");
-  currenciesPara.textContent = `Currencies: ${currencyNames.join(
-    ", "
-  )} (${currencySymbols.join(", ")})`;
+  if (currencies) {
+    const currencyNames = Object.values(currencies).map(
+      (currency) => currency.name
+    );
+    const currencySymbols = Object.values(currencies).map(
+      (currency) => currency.symbol
+    );
+    infoDiv.appendChild(
+      createParagraph(
+        "currencies",
+        `Currencies: ${currencyNames.join(", ")} (${currencySymbols.join(
+          ", "
+        )})`
+      )
+    );
+  }
 
-  const languageCodes = Object.keys(languages);
-  const languageNames = Object.values(languages);
-
-  const languagesPara = document.createElement("p");
-  languagesPara.textContent =
-    "Languages: " +
-    languageCodes.map((code, index) => `${languageNames[index]}`).join(", ");
+  if (languages) {
+    const languageCodes = Object.keys(languages);
+    const languageNames = Object.values(languages);
+    infoDiv.appendChild(
+      createParagraph(
+        "languages",
+        "Languages: " +
+          languageCodes
+            .map((code, index) => `${languageNames[index]}`)
+            .join(", ")
+      )
+    );
+  }
 
   const borderCountriesPara = document.createElement("p");
   borderCountriesPara.textContent = "Border Countries: ";
@@ -189,15 +214,6 @@ const showModal = (element, filteredData) => {
     console.log("borders dizisi tanımlı değil veya undefined.");
   }
 
-  infoDiv.appendChild(countryName);
-  infoDiv.appendChild(nativeName);
-  infoDiv.appendChild(populationPara);
-  infoDiv.appendChild(regionPara);
-  infoDiv.appendChild(subRegionPara);
-  infoDiv.appendChild(capitalPara);
-  infoDiv.appendChild(domainPara);
-  infoDiv.appendChild(currenciesPara);
-  infoDiv.appendChild(languagesPara);
   infoDiv.appendChild(borderCountriesPara);
   infoDiv.appendChild(borderList);
 
@@ -205,42 +221,28 @@ const showModal = (element, filteredData) => {
   modalContent.appendChild(infoDiv);
 
   modal.style.display = "block";
-  countries.style.display = "none";
-  filter.style.display = "none";
+  countriesContainer.style.display = "none";
 };
 
-// Modal Kapamak için
 const closeModal = () => {
-  const modal = document.getElementById("modal");
-  const countries = document.querySelector(".countries");
-  const filter = document.querySelector(".filters");
   modal.style.display = "none";
-  countries.style.display = "flex";
-  filter.style.display = "flex";
+  countriesContainer.style.display = "flex";
 };
 
 document.addEventListener("DOMContentLoaded", countryApi);
 
 document.addEventListener("click", function (event) {
-  if (event.target === document.getElementById("modal")) {
+  if (event.target === modal) {
     closeModal();
   }
 });
 
 backButton.addEventListener("click", closeModal);
 
-// Dark Mode için
 const darkMode = () => {
-  document.addEventListener("DOMContentLoaded", function () {
-    const toggleSwitch = document.querySelector("#toggle");
-
-    toggleSwitch.addEventListener("change", function () {
-      if (this.checked) {
-        document.body.classList.add("dark-mode");
-      } else {
-        document.body.classList.remove("dark-mode");
-      }
-    });
+  const toggleSwitch = document.querySelector("#toggle");
+  toggleSwitch.addEventListener("change", function () {
+    document.body.classList.toggle("dark-mode", this.checked);
   });
 };
 
